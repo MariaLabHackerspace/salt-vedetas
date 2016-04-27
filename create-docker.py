567412c2cdfd95f0f14d963208683e2c6e2ba0e8
Aline Freitas <aline@alinefreitas.com.br>
2016-04-27 01:22:16 -0300

    Add our function inside a class for better code organization. Also added some comments/descriptions.

--- create-docker.py
+++ create-docker.py
@@ -3,46 +3,66 @@
 import argparse
 import yaml
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Create yaml docker for salt.')
-    parser.add_argument('-n', '--container_name', required=True)
-    parser.add_argument('-v', '--volumes')
-    # parser.add_argument('-l', '--links', help='Link container to existing one.')
-    # parser.add_argument('-p', '--ports')
-    parser.add_argument('image')
-
-    args = parser.parse_args()
-    return args
-
-def default_dict():
-    docker_dict = dict((key, value) for key, value in vars(get_args()).iteritems() if value is not None)
-    docker_dict['restart'] = 'always'
-    full_dict = {'docker': {'compose': { get_args().container_name : docker_dict}}}
-    return full_dict
-
-def write_header():
-    header = '{% import_yaml "docker/secret.sls" as secret %}\n\n'
-    with open("{}.sls".format(get_args().container_name), 'w') as docker_file:
-        docker_file.write(header)
-
-def write_yaml():
-    write_header()
-    with open("{}.sls".format(get_args().container_name), 'a') as docker_file:
-        docker_file.write(yaml.dump(default_dict(), default_flow_style=False))
-
-def write_extras():
-    write_yaml()
-    extras = """
-      {#
-      links: 
-        - 'name:alias' 
-      ports:
-        - '80:80' 
-      #}
+class CreateDocker(object):
     """
-    with open("{}.sls".format(get_args().container_name), 'a') as docker_file:
-        docker_file.write(extras)
+        We generate a sls file for docker, ready to use for salt.
+    """
+
+    def get_args(self):
+        """
+            Get command line arguments
+        """
+        parser = argparse.ArgumentParser(description='Create yaml docker for salt.')
+        parser.add_argument('-n', '--container_name', required=True)
+        parser.add_argument('-v', '--volumes')
+        # parser.add_argument('-l', '--links', help='Link container to existing one.')
+        # parser.add_argument('-p', '--ports')
+        parser.add_argument('image')
+
+        args = parser.parse_args()
+        return args
+
+    def default_dict(self):
+        """
+            We put our args inside docker->compose.
+        """
+        docker_dict = dict((key, value) for key, value in vars(self.get_args()).iteritems() if value is not None)
+        docker_dict['restart'] = 'always'
+        full_dict = {'docker': {'compose': { self.get_args().container_name : docker_dict}}}
+        return full_dict
+
+    def write_header(self):
+        """
+            docker/secret.sls is were we store sensitive data.
+        """
+        header = '{% import_yaml "docker/secret.sls" as secret %}\n\n'
+        with open("{}.sls".format(self.get_args().container_name), 'w') as docker_file:
+            docker_file.write(header)
+
+    def write_yaml(self):
+        """
+            Write command line arguments into yaml.
+        """
+        self.write_header()
+        with open("{}.sls".format(self.get_args().container_name), 'a') as docker_file:
+            docker_file.write(yaml.dump(self.default_dict(), default_flow_style=False))
+
+    def write_extras(self):
+        """
+            Commented out extra arguments.
+        """
+        self.write_yaml()
+        extras = """
+          {#
+          links: 
+            - 'name:alias' 
+          ports:
+            - '80:80' 
+          #}
+        """
+        with open("{}.sls".format(self.get_args().container_name), 'a') as docker_file:
+            docker_file.write(extras)
 
 if __name__ == "__main__":
-    write_extras()
-    # print get_args().container_name
+    create_docker = CreateDocker()
+    create_docker.write_extras()
